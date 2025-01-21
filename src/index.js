f06fef23661d898c08eef12ebfe4068da7521656
dunglt94 <tuandung9594@gmail.com>
2025-01-21 11:50:20 +0700

    add component App2

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import App2 from "./components/App2";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
+);
+
+const root2 = ReactDOM.createRoot(document.getElementById('root2'));
+root2.render(
+    <App2 />
 );
